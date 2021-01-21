da3f1f998ca25c7ac3c3204f8cec9525a5e3cd20
oujie <1277053320@qq.com>
2021-01-21 13:03:49 +0800

    修改yml文件

---
+++ LookProject/src/main/java/com/iswust/SpringBootApplicationLook.java
@@ -0,0 +1,11 @@
+package com.iswust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationLook {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationLook.class);
+    }
+}
