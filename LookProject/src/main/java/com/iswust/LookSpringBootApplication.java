93828ffb926e3416cc11f11d08954eb62555e785
oujie <1277053320@qq.com>
2021-01-21 11:02:34 +0800

    首次提交  基本结构

---
+++ LookProject/src/main/java/com/iswust/LookSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.iswust;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LookSpringBootApplication.class);
+    }
+}
